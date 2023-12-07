0e2c7f02887b485889cd4e988820a6e533b54354
jfadev <jfadev@gmail.com>
2023-12-07 15:55:23 +0100

    Fix multi-task.bat

--- multi-task.bat
+++ multi-task.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 REM Define initial variables
 set counter=1
@@ -8,7 +9,7 @@ if not defined filename set "filename=tasks.csv"
 REM Read the CSV file line by line and assign the values to variables
 for /F "tokens=1,2,3 delims=," %%a in (%filename%) do (
   REM Execute the command with the corresponding variables
-  start cmd /c node bottok.js ^
+  start /b cmd /c node bottok.js ^
     -d ^
     -o ^
     -c cookies!counter!.json ^
