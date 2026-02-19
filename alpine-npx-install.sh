0e6b088862c8c788579414e46bf6b537a187f67a
Jordi Fernandes Alves <jfadev@gmail.com>
2026-02-19 16:35:14 +0100

    Fix

--- alpine-npx-install.sh
+++ alpine-npx-install.sh
@@ -5,7 +5,7 @@ echo "Bot Tok Alpine Install (for NPX)"
 echo "Install BotTok Dependencies:"
 
 # Install Node from Alpine v3.22 repositories
-apk add --repository=https://dl-cdn.alpinelinux.org/alpine/v3.22/main --repository=https://dl-cdn.alpinelinux.org/alpine/v3.22/community chromium --no-cache
+apk add --repository=https://dl-cdn.alpinelinux.org/alpine/v3.22/main --repository=https://dl-cdn.alpinelinux.org/alpine/v3.22/community nodejs npm --no-cache
 
 # Install Chromium from Alpine v3.17 repositories
 apk add --repository=https://dl-cdn.alpinelinux.org/alpine/v3.17/main --repository=https://dl-cdn.alpinelinux.org/alpine/v3.17/community chromium --no-cache
